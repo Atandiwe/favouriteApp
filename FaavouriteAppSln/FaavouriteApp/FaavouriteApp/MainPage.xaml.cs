f81e99bb1ea3dbf843a217ed289eb86b1170013e
Atandiwe <102726464+Atandiwe@users.noreply.github.com>
2022-05-11 10:37:28 +0200

    added start up code

---
+++ FaavouriteAppSln/FaavouriteApp/FaavouriteApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FaavouriteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Contacts_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Contacts());
+        }
+    }
+}
